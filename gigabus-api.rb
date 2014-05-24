6c57888514e9cbb5b48ac4b8c9220a4626f25b8b
Drew Carey Buglione <me@drewb.ug>
2014-05-24 12:27:53 -0400

    Initial commit

---
+++ gigabus-api.rb
@@ -0,0 +1,31 @@
+#!/usr/bin/env ruby
+
+require 'rubygems'
+require 'bundler/setup'
+
+require 'json'
+
+require 'faraday'
+
+DEPARTURE_JS_START = "    var departureCities = new Array(); \n"
+DEPARTURE_JS_END = "      \n"
+
+js = Faraday.get('http://m.saucontds.com/fc/megabususa.htm').body.lines.drop_while do |line|
+  line != DEPARTURE_JS_START
+end.take_while do |line|
+  line != DEPARTURE_JS_END
+end.join
+
+departure_cities = js.scan(/departureCities\['.*'\] = {"name": "(.+)"};/)[1..-1].map(&:first)
+departure_routes = Hash[departure_cities.map { |name| [name, js.scan(/departureCities\['#{name}'\].destination\[\d+\] = {"companyId": \d+, "name": "(.+)"};/).map(&:first)] }]
+
+arrival_cities = departure_routes.values.flatten!(1).uniq!
+arrival_routes = Hash[arrival_cities.map { |name| [name, departure_routes.select{|_,v|v.include?(name)}.keys] }]
+
+File.open('departure_routes.json', 'w') do |file|
+  file.write JSON.pretty_generate(departure_routes)
+end
+
+File.open('arrival_routes.json', 'w') do |file|
+  file.write JSON.pretty_generate(arrival_routes)
+end
